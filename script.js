b8f55f25dc3db4dacc1c18d2040ad5e96f6397e1
Mananpreet Kaur <manankaur00@gmail.com>
2020-12-11 04:21:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-//Client site file
 
 const socket = io('http://localhost:3000')
+//creates a new socket instance attached to the http server.
+
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
 const name = prompt('What is your name?')
 appendMessage('You joined')
+
+//custom event
 socket.emit('new-user', name)
 
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
 })
 
+//handling event
 socket.on('user-connected', name => {
     appendMessage(`${name} connected`)
 })
@@ -22,9 +26,11 @@ socket.on('user-disconnected', name => {
 })
 
 messageForm.addEventListener('submit', e => {
+    //stops our page from refreshing
     e.preventDefault()
     const message = messageInput.value
     appendMessage(`You: ${message}`)
+    //custom event
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
